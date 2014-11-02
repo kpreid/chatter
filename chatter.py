b12fb1d0c034ddc819d374e95d6b71667b68dfd0
Kevin Reid <kpreid@switchb.org>
2014-11-01 18:20:34 -0700

    Cleanup, license.

--- chatter.py
+++ chatter.py
@@ -1,47 +1,53 @@
 #!/usr/bin/env python
 
-import sys, os, random, time
+# Copyright 2010, 2014 Kevin Reid
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import os, random, time
 
 voices = [
-"Agnes",
-"Albert",
-"Alex",
-#"Bad News",
-"Bahh",
-#"Bells",
-"Boing",
-"Bruce",
-"Bubbles",
-#"Cellos",
-"Deranged",
-"Fred",
-#"Good News",
-#"Hysterical",
-#"Junior",
-#"Kathy",
-#"Pipe Organ",
-#"Princess",
-"Ralph",
-#"Trinoids",
-"Vicki",
-#"Victoria",
-"Whisper",
-"Zarvox"]
+    "Agnes",
+    "Albert",
+    "Alex",
+    "Bahh",
+    "Boing",
+    "Bruce",
+    "Bubbles",
+    "Deranged",
+    "Fred",
+    "Ralph",
+    "Vicki",
+    "Whisper",
+    "Zarvox"
+]
 
-#voices = ["Alex"]
-
+# TODO: Might want to omit some words / use a different list.
 words = [x.rstrip().lower() for x in open("/usr/share/dict/words")]
 
-while True:
-  voice = random.choice(voices)
-  #voice = "Vicki"
-  text = " ".join("".join(random.sample(words, 4)) for x in range(1, 5))
-  print voice, "-", text
-  os.spawnlp(os.P_NOWAIT, "say", "say", "-v", voice, "[[rate 1]]", text)
-  time.sleep(10)
+words_per_word = 4
+words_per_voice = 5
+commands = "[[rate 1]]"
 
-# notes:
-# Halloween ambient voice generator
-#   generate text (source corpus? Markov-chain? quotes? dialogue? Lojban?)
-#   insert speech commands (wobble rate, ...)
-#   speak in random voice
+while True:
+    voice = random.choice(voices)
+    text = " ".join("".join(random.sample(words, words_per_word)) for _ in xrange(words_per_voice))
+    print voice, "-", text
+    os.spawnlp(os.P_NOWAIT, "say", "say", "-v", voice, commands, text)
+    time.sleep(10)
